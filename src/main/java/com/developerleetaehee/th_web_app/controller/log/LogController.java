84518c56d894ad8493e3a6d015ccfc0d6c4f6948
leetaehee <lastride25@naver.com>
2025-12-09 18:32:02 +0900

    도커 로그 작업 가능하도록 분리

--- src/main/java/com/developerleetaehee/th_web_app/controller/log/LogController.java
+++ src/main/java/com/developerleetaehee/th_web_app/controller/log/LogController.java
@@ -1,22 +1,40 @@
 package com.developerleetaehee.th_web_app.controller.log;
 
+import com.developerleetaehee.th_web_app.common.MenuCustomCode;
+import com.developerleetaehee.th_web_app.common.MenuInfo;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/log")
 public class LogController {
 
     private static final Logger logger = LoggerFactory.getLogger(LogController.class);
 
+    private final MenuCustomCode menuCustomCode;
+
     @GetMapping("/logger")
-    public String loggerTest() {
+    public String loggerTest(Model model) {
 
-        logger.info("깃허브에 업로드해놓고 도커 테스트 ㅋ");
+        logger.info("도커 로그 테스트 100");
+        logger.info("도커 로크 테스트 200");
+        logger.info("도커 로크 테스트 300");
 
-        return "깃허브에 업로드해놓고 도커 테스트 ㅋ";
+        MenuInfo menuInfo = menuCustomCode.getCode().get("log_logger_test");
+
+        if (menuInfo == null) {
+            throw new IllegalArgumentException("존재하지 않는 게시판 유형입니다. 관리자에게 문의하세요.");
+        }
+
+        model.addAttribute("title", menuInfo.getTitle());
+        model.addAttribute("menuCustomCode", menuInfo);
+
+        return "shop/product_list";
     }
 }
